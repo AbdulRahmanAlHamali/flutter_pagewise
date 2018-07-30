0acb4248539365b5cd513043d1fbb06eff160495
aalhamali <a.alhamali@saugo360.com>
2018-07-31 01:12:27 +0300

    add lazy-loading to library description

--- lib/flutter_pagewise.dart
+++ lib/flutter_pagewise.dart
@@ -1,4 +1,4 @@
-/// A library for widgets that load their content one page (or batch) at a time.
+/// A library for widgets that load their content one page (or batch) at a time (also known as lazy-loading).
 ///
 /// The library provides three widgets:
 ///  * [Pagewise]: An abstract widget that pagewise widgets must extend and
