0fc274ad0b7e6f62706979d9f673ed249350bef7
Mohammed Fahjan <fahjan@gmail.com>
2022-09-12 22:46:17 +0300

    change FlatButton to TextButton
    comment color and shape

--- lib/flutter_pagewise.dart
+++ lib/flutter_pagewise.dart
@@ -309,13 +309,13 @@ class PagewiseState<T> extends State<Pagewise<T>> {
   }
 
   Widget _getRetryWidget() {
-    var defaultRetryButton = FlatButton(
+    var defaultRetryButton = TextButton(
       child: Icon(
         Icons.refresh,
         color: Colors.white,
       ),
-      color: Colors.grey[300],
-      shape: CircleBorder(),
+//       color: Colors.grey[300],
+//       shape: CircleBorder(),
       onPressed: this._effectiveController!.retry,
     );
 
